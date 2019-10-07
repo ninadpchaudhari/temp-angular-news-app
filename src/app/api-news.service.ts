aae3f61ef7a6c1e059c6d870c4d4d4f90cf56709
Ninad P.Chaudhari <nchaudhari@albany.edu>
2019-10-07 12:07:56 -0400

    Added  the get request method.

--- src/app/api-news.service.ts
+++ src/app/api-news.service.ts
@@ -1,10 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ApiNewsService {
 
+export class ApiNewsService {
+  
+  API_KEY:string ="e06f5b3da2be4501af757078cf03a985";
   constructor(private httpClient: HttpClient) { }
+
+  public getNews(){
+    return this.httpClient.get<Config>(`https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${this.API_KEY}`);
+  }
+}
+export interface Config {
+  heroesUrl: string;
+  textfile: string;
 }
