ec0ae04ad53d65acf66937449cbebe988ff35365
Ninad P.Chaudhari <ninadpchaudhari@gmail.com>
2019-10-22 00:16:32 -0400

    Change news endpoint parameters

--- src/app/api-news.service.ts
+++ src/app/api-news.service.ts
@@ -12,7 +12,7 @@ export class ApiNewsService {
   constructor(private httpClient: HttpClient) { }
 
   public getNews(){
-    return this.httpClient.get<Config>(`https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${this.API_KEY}`);
+    return this.httpClient.get<Config>(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${this.API_KEY}`);
   }
 }
 
